ae411179bd06a36d3404b97c3b3d82b1c2d7980b
Raymond Brady <rbrady@webextant.com>
2024-07-22 05:06:39 -0500

    add mutex to ensure only one instance of the app can run

--- BabyGame/Program.cs
+++ BabyGame/Program.cs
@@ -1,3 +1,13 @@
-﻿
+﻿using System.Threading;
+using System;
+
+using var mutex = new Mutex(true, "BabyGameSingletonMutex", out bool isNewInstance);
+
+if (!isNewInstance)
+{
+    Console.WriteLine("Another instance of the application is already running.");
+    return;
+}
+
 using var game = new BabyGame.BabyGame();
 game.Run();
